77e5a99a32d0d5a28b25bc44b374868a57d2bc52
kimjunghyun <hide1989@naver.com>
2020-09-13 11:32:29 +0900

    spring part1 END

--- src/main/java/org/hide1989/mapper/TimeMapper.java
+++ src/main/java/org/hide1989/mapper/TimeMapper.java
@@ -7,6 +7,8 @@ public interface TimeMapper {
 	
 	@Select("Select sysdate FROM dual")
 	public String getTime();
+	
+	public String getTime2();
 
 }
 //Mapper를 작성하는 작업은 XML을 이용할 수도 있지만, 이번에는 최소한의 코드를 작성하는 Mapper 인터페이스를 사용한다.
